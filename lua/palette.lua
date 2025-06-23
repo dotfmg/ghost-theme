b11604e66216538a6929fdb9430b91b641aa2632
Franklin Garcia <fm-garcia@outlook.com>
2025-06-23 18:12:38 -0500

    feat(theme): integrate palette module and enhance highlight groups

---
+++ lua/palette.lua
@@ -0,0 +1,13 @@
+return {
+  bg = "#25272D",
+  fg = "#FFFFFF",
+  comment = "#5c6370",
+  keyword = "#c678dd",
+  string = "#98c379",
+  func = "#61afef",
+  variable = "#e06c75",
+  type = "#e5c07b",
+  number = "#d19a66",
+  constant = "#56b6c2",
+  parameter = "#d19a66",
+}
